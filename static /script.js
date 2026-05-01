2067d19a1729c76bec4694a660d4c4651695b4e9
zenlopes2910-cmyk <zenlopes2910@gmail.com>
2026-05-01 12:44:58 +0530

    Update script.js

--- static /script.js
+++ static /script.js
@@ -2,3 +2,11 @@
 function openDetails(house) {
     window.location.href = "/details/" + house;
 }
+
+function openDetails(house) {
+    window.location.href = "/details/" + house;
+}
+
+function viewMembers(house) {
+    window.location.href = "/members/" + house;
+}
